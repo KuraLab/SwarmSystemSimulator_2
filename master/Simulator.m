7bdc67c2473b4f71f4541a0f9beb33ad7d248c80
YukiOrigane <origane.y.aa@m.titech.ac.jp>
2022-11-28 11:41:36 +0900

    Add function of parameter

---
+++ master/Simulator.m
@@ -0,0 +1,51 @@
+classdef Simulator
+    %SIMULATOR シミュレータでよく使う機能をまとめたクラス
+    %       パラメータの変更機能，動画の撮影機能などを入れる予定．継承して色々なシミュレータに利用してほしい
+    
+    properties
+        param   % パラメータの入った構造体オブジェクト
+    end
+    
+    methods
+        function obj = Simulator()
+            % コンストラクタ
+            obj.param = struct;   % paramオブジェクトの生成
+            obj = obj.setDefaultParameters();
+        end
+        
+        function obj = setDefaultParameters(obj)
+            % パラメータとデフォルト値を設定．継承で好きにいじってほしい
+            obj.param.dt = 0.01;
+        end
+
+        function obj = setParam(obj, field, value)
+            % 1つのパラメターを変更．フィールド名，ないものは警告出す．外から使う
+            arguments
+                obj             % Simulatorオブジェクト
+                field string      % パラメータ名（文字ベクトル）
+                value = 0       % パラメータ初期値．省略なら0が入る
+            end
+            if isfield(obj.param, field)
+                obj.param.field = value;
+            else
+                disp("WARNIG：パラメータに "+field+" は存在しません");
+            end
+        end
+
+        function obj = setMultiParam(obj, list)
+            arguments
+                obj         % Simulatorオブジェクト
+                list        % フィールドと値が並んだ配列．["Nt", 10, "use_CBF", true] みたいに使う
+            end
+            if mod(length(list),2) == 0
+                paramN = length(list)/2;
+                for n = 1:paramN
+                    obj = obj.setParam(list(2*n-1), list(2*n));
+                end
+            else
+                disp("WARNIG : パラメータリストの長さは，2の整数倍である必要があります");
+            end
+        end
+    end
+end
+
