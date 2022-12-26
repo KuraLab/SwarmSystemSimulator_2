e9a434b1ff1e0fcd3160a696049076211b4d3f5c
YukiOrigane <origane.y.aa@m.titech.ac.jp>
2022-12-26 17:38:59 +0900

    add deSitter

--- time_space/simple_time_space_potential/simpleTimeSpace.m
+++ time_space/simple_time_space_potential/simpleTimeSpace.m
@@ -20,6 +20,7 @@ classdef simpleTimeSpace < Simulator
             obj.param.Ntau = 400;    % 計算するカウント数
             obj.param.tsp_dim = 2;      % 時空間の次元
             %%%%%%%% システムパラメータ %%%%%%%%
+            obj.param.metric = "const_input";    % 計量の形
             obj.param.A = 2;    % 定数1
             obj.param.B = 1;    % 定数2
             obj.param.C = 1;    % 定数3
@@ -42,8 +43,16 @@ classdef simpleTimeSpace < Simulator
 
         function obj = defineSystem(obj)
             % システムの定義が必要な場合はここ．シミュレーションをやり直すたびに呼ぶこと
-            obj.tspace = constantInputMetric(obj.param.tsp_dim);
-            obj.tspace = obj.tspace.setMetric(obj.param.A, obj.param.B, obj.param.C, obj.param.f, obj.param.c);
+            if (obj.param.metric == "const_input")
+                % 定数入力
+                obj.tspace = constantInputMetric(obj.param.tsp_dim);
+                obj.tspace = obj.tspace.setMetric(obj.param.A, obj.param.B, obj.param.C, obj.param.f, obj.param.c);
+            end
+            if (obj.param.metric == "deSitter")
+                % ドジッター空間に入力
+                obj.tspace = deSitterMetric(obj.param.tsp_dim);
+                obj.tspace = obj.tspace.setMetric(obj.param.f, obj.param.c);
+            end
         end
 
         function obj = simulate(obj)
@@ -54,7 +63,7 @@ classdef simpleTimeSpace < Simulator
                 Gamma2 = obj.tspace.calcChristoffelNumWithoutpartialG(obj.q(:,tau));
                 obj.q(:,tau+1) = obj.q(:,tau) + obj.param.dtau*obj.dqdtau(:,tau);
                 dxdtau = obj.dqdtau(:,tau)*obj.dqdtau(:,tau).'; % テンソル積の計算を楽にするために，行列にしておく
-                dx2dtau2 = tensorprod(Gamma2,dxdtau,[1 2],[1 2]); % テンソル籍の計算
+                dx2dtau2 = -tensorprod(Gamma2,dxdtau,[1 2],[1 2])+[0;obj.param.f]; % テンソル籍の計算
                 obj.dqdtau(:,tau+1) = obj.dqdtau(:,tau) + obj.param.dtau*dx2dtau2;
             end % for
             toc
@@ -66,21 +75,57 @@ classdef simpleTimeSpace < Simulator
 
             figure  % 生値プロット
             plot(obj.tau_vec, obj.q(:,:));
-            title("状態の固有時間変化")
+            title("状態の固有時間変化 (c="+string(obj.param.c)+", dx/d\tau(0) = ["+string(obj.param.dqdtau_0(1))+", "+string(obj.param.dqdtau_0(2))+"])")
             xlabel("固有時刻 \tau [s]")
             legend(["x^0" "x^1"])
 
             figure;  % 生値プロット
             plot(obj.q(1,:)/obj.param.c, obj.q(2,:));
-            title("状態の世界時間変化")
+            title("状態の世界時間変化 (c="+string(obj.param.c)+", dx/d\tau(0) = ["+string(obj.param.dqdtau_0(1))+", "+string(obj.param.dqdtau_0(2))+"])")
             ylabel("x^1")
             xlabel("時刻 t [s]")
             %legend(string(1:obj.param.Na))
         end
 
         function obj = subplot(obj)
+            % 状態の世界時間プロット
             plot(obj.q(1,:)/obj.param.c, obj.q(2,:));
             %legend(string(1:obj.param.Na))
+        end
+
+        function obj = subplot_eigentime(obj,dim)
+            % 状態の固有時間プロット
+            arguments
+                obj
+                dim % プロットする次元
+            end
+            plot(obj.tau_vec, obj.q(dim,:));
+            %legend(string(1:obj.param.Na))
+        end
+
+        function obj = showEigen(obj, vecdim1, vecdim2)
+            % 固有値と固有ベクトルをグリッドの各点毎に表示
+            arguments
+                obj
+                vecdim1 % 値を計算するグリッド点のdim1方向の集合
+                vecdim2 % 値を計算するグリッド点のdim2方向の集合
+            end
+            %figure
+            v1 = zeros(2,length(vecdim1)*length(vecdim2));   % ベクトルの集合
+            v2 = zeros(2,length(vecdim1)*length(vecdim2));
+            x1 = zeros(2,length(vecdim1)*length(vecdim2));   % 位置の集合
+            for n1 = 1:length(vecdim1)
+                for n2 = 1:length(vecdim2)
+                    G = obj.tspace.calcMetricNum([obj.param.c*vecdim1(n1);vecdim2(n2)]);
+                    [V,D] = eig(G);
+                    v1(:,(n1-1)*length(vecdim2)+(n2-1)+1) = D(1,1)*V(:,1);
+                    v2(:,(n1-1)*length(vecdim2)+(n2-1)+1) = D(2,2)*V(:,2);
+                    x1(:,(n1-1)*length(vecdim2)+(n2-1)+1) = [vecdim1(n1);vecdim2(n2)];
+                end
+            end
+            quiver(x1(1,:),x1(2,:),v1(1,:),v1(2,:),'LineWidth',1.5);
+            hold on
+            quiver(x1(1,:),x1(2,:),v2(1,:),v2(2,:),'LineWidth',1.5);
         end
 
     end % methods
